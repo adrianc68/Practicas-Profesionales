2865dab9587144d936392576595b360eb93b7d78
Jose Luis Carvajal Pacheco <58019262+JLUISCP@users.noreply.github.com>
2020-05-08 17:29:09 -0500

    CoordinatorDAO test

--- src/test/org/database/dao/ProfessorDAOTest.java
+++ src/test/org/database/dao/ProfessorDAOTest.java
@@ -1,6 +1,9 @@
 package test.org.database.dao;
 
+import org.database.dao.CoordinatorDAO;
 import org.database.dao.ProfessorDAO;
+import org.domain.Course;
+import org.domain.Professor;
 import org.junit.FixMethodOrder;
 import org.junit.jupiter.api.Test;
 import org.junit.runners.MethodSorters;
@@ -9,12 +12,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ProfessorDAOTest {
-    private final int idProfessor = 3;
     private final int idPractitioner = 3;
 
     @Test
     void addProfessor() {
-
+        Professor professor = new Professor();
+        professor.setName("Rafael Lira Santos");
+        professor.setPhoneNumber("2281569492");
+        professor.setEmail("rafaellirasa@hotmail.com");
+        professor.setCubicle(25);
+        professor.setStaffNumber("0001-A1");
+        Course course = new Course();
+        course.setId(1);
+        professor.setCourse(course);
+        ProfessorDAO professorDAO = new ProfessorDAO();
+        int expected = 1;
+        int actual = professorDAO.addProfessor(professor);
+        System.out.println(actual);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -33,7 +48,9 @@ public class ProfessorDAOTest {
 
     @Test
     void removeProfessor() {
-
+        ProfessorDAO professorDAO = new ProfessorDAO();
+        boolean actual = professorDAO.removeProfessor(1);
+        assertTrue(actual);
     }
 
 }
