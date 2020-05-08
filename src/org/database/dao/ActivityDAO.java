a3528696b07119ca3086dddef0c48e9e13d237b5
Jose Luis Carvajal Pacheco <58019262+JLUISCP@users.noreply.github.com>
2020-05-08 17:11:25 -0500

    Professor and Practitioner fectures

--- src/org/database/dao/ActivityDAO.java
+++ src/org/database/dao/ActivityDAO.java
@@ -1,27 +1,150 @@
 package org.database.dao;
 
+import org.database.Database;
 import org.domain.Activity;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ActivityDAO implements IActivityDAO {
-    @Override
-    public boolean addActivity(Activity activity) {
-        return true;
+    /***
+     * Constant for the connection to the database
+     */
+    private final Database database;
+    /***
+     * Query results
+     */
+    private ResultSet resultSet;
+
+    /***
+     * ActivityDAO constructor.
+     * This constructor initialize a connection to the database.
+     */
+    public ActivityDAO() {
+        this.database = new Database();
     }
 
+    /***
+     * Add an activity to database.
+     * <p>
+     * This method is used by Professor when he/she needs to add an activity.
+     * </p>
+     * @param activity to be added to database.
+     * @return int representing the activity's id.
+     */
+    @Override
+    public int addActivity(Activity activity) {
+        int idActivity = 0;
+        try(Connection conn = database.getConnection() ){
+            conn.setAutoCommit(false);
+            String statement = "INSERT INTO Activity(name, description, deadline, id_professor) VALUES (?,?,?,?)";
+            PreparedStatement insertActivity = conn.prepareStatement(statement);
+            insertActivity.setString(1, activity.getName());
+            insertActivity.setString(2, activity.getDescription());
+            insertActivity.setDate(3, activity.getDeadline());
+            insertActivity.setInt(4, activity.getProfessor().getId());
+            insertActivity.executeUpdate();
+            statement = "SELECT LAST_INSERT_ID()";
+            insertActivity = conn.prepareStatement(statement);
+            resultSet = insertActivity.executeQuery();
+            resultSet.next();
+            idActivity = resultSet.getInt(1);
+            conn.commit();
+        } catch (SQLException | NullPointerException e) {
+            Logger.getLogger(ActivityDAO.class.getName()).log(Level.SEVERE, null, e);
+        }
+        return idActivity;
+    }
+
+    /***
+     * Remove an activity from database.
+     * <p>
+     * This method is used by Professor when he/she needs to remove an activity.
+     * </p>
+     * @param idActivity to be removed to database
+     * @return boolean true if 1 o more than 1 rows are affected
+     */
     @Override
     public boolean removeActivityByID(int idActivity) {
-        return true;
+        int rowsAffected = 0;
+        try(Connection conn = database.getConnection()){
+            conn.setAutoCommit(false);
+            String statement = "DELETE FROM Activity WHERE id_activity = ?";
+            PreparedStatement removeActivity = conn.prepareStatement(statement);
+            removeActivity.setInt(1, idActivity );
+            rowsAffected = removeActivity.executeUpdate();
+            conn.commit();
+        } catch (SQLException | NullPointerException e) {
+            Logger.getLogger(ActivityDAO.class.getName()).log(Level.SEVERE, null, e);
+        }
+        return rowsAffected > 0;
     }
 
+    /***
+     * Get all activities from the actual/last course
+     * <p>
+     * This method is used by the Professor when he/she needs to look up all the activities
+     * </p>
+     * @return List<activities> a list with activities of the actual/last course from database
+     */
     @Override
     public List<Activity> getAllActivitiesFromLastCourse() {
-        return null;
+        List<Activity> activities = new ArrayList<>();
+        try(Connection conn = database.getConnection() ) {
+            String statement = "SELECT ACT.id_activity, ACT.name, ACT.description, ACT.deadline FROM activity AS ACT INNER JOIN professor AS PROF ON ACT.id_professor = PROF.id_person INNER JOIN person AS PER ON PER.id_person = PROF.id_person INNER JOIN course AS COUR ON COUR.id_course = PER.id_course AND COUR.id_course = (SELECT max(id_course) FROM Course)";
+            PreparedStatement queryActivity = conn.prepareStatement(statement);
+            resultSet = queryActivity.executeQuery();
+            while( resultSet.next() ) {
+                Activity activity = new Activity();
+                activity.setName(resultSet.getString("ACT.name"));
+                activity.setDescription(resultSet.getString("ACT.description"));
+                activity.setDeadline(resultSet.getDate("ACT.deadline"));
+                activity.setId((resultSet.getInt("ACT.id_activity")));
+                activity.setDeliveries(null);
+                activity.setProfessor(null);
+                activities.add(activity);
+            }
+        } catch (SQLException | NullPointerException e) {
+            Logger.getLogger(ActivityDAO.class.getName()).log(Level.WARNING, null, e);
+        }
+        return activities;
     }
 
+    /***
+     * Get all activities looking for Professor.
+     * <p>
+     * This method is used by the Practitioner and Professor.
+     * </p>
+     * @param idProfessor the professor's id
+     * @return List<activities> a list with activities looking for Professor.
+     */
     @Override
-    public List<Activity> getAllActivitiesByProfessor() {
-        return null;
+    public List<Activity> getAllActivitiesByProfessorID(int idProfessor){
+        List<Activity> activities = new ArrayList<>();
+        try(Connection conn = database.getConnection() ) {
+            String statement = "SELECT ACT.id_activity, ACT.name, ACT.description, ACT.deadline FROM Activity AS ACT WHERE ACT.id_professor = ?";
+            PreparedStatement queryActivity = conn.prepareStatement(statement);
+            queryActivity.setInt(1, idProfessor );
+            ResultSet result = queryActivity.executeQuery();
+            while( result.next() ) {
+                Activity activity = new Activity();
+                activity.setName(result.getString("ACT.name"));
+                activity.setDescription(result.getString("ACT.description"));
+                activity.setDeadline(result.getDate("ACT.deadline"));
+                activity.setId((result.getInt("ACT.id_activity")));
+                activity.setDeliveries(null);
+                activity.setProfessor(null);
+                activities.add(activity);
+            }
+        } catch (SQLException | NullPointerException e) {
+            Logger.getLogger(ActivityDAO.class.getName()).log(Level.WARNING, null, e);
+        }
+        return activities;
     }
 
 }
--- src/org/database/dao/DeliveryDAO.java
+++ src/org/database/dao/DeliveryDAO.java
@@ -1,12 +1,13 @@
 package org.database.dao;
 
 import org.database.Database;
-import org.domain.*;
-
+import org.domain.Activity;
+import org.domain.Course;
+import org.domain.Delivery;
+import org.domain.Professor;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -18,25 +19,91 @@ import java.util.logging.Logger;
 
 public class DeliveryDAO implements IDeliveryDAO {
     private final Database database;
-    private ResultSet result;
+    private ResultSet resultSet;
 
+    /***
+     * DeliveryDAO constructor.
+     * This constructor initialize a connection to the database.
+     */
     public DeliveryDAO() {
         database = new Database();
     }
 
+    /***
+     * Add a Delivery to database.
+     * <p>
+     * This method is used by the Practitioner when he/she needs to add a Delivery.
+     * </p>
+     * @param delivery, idActivity
+     * @return int representing the delivery's id
+     */
     @Override
-    public boolean addDeliveryActivity() {
-        return true;
+    public int addDeliveryToActivity(Delivery delivery, int idActivity) {
+        int idDelivery = 0;
+        try(Connection conn = database.getConnection() ){
+            conn.setAutoCommit(false);
+            String statement = "CALL addDelivery(?,?,?,?)";
+            PreparedStatement addDelivery = conn.prepareStatement(statement);
+            addDelivery.setInt(1,idActivity);
+            addDelivery.setInt( 2, delivery.getPractitioner().getId() );
+            File file = delivery.getDocument().getDocument();
+            FileInputStream fileInputStream = new FileInputStream(file);
+            addDelivery.setBinaryStream(3, fileInputStream);
+            addDelivery.setString(4, delivery.getDocument().name() );
+            addDelivery.executeUpdate();
+            statement = "SELECT LAST_INSERT_ID()";
+            addDelivery = conn.prepareStatement(statement);
+            resultSet = addDelivery.executeQuery();
+            resultSet.next();
+            idDelivery = resultSet.getInt(1);
+            conn.commit();
+        } catch (SQLException | NullPointerException | FileNotFoundException e) {
+            Logger.getLogger(DeliveryDAO.class.getName()).log(Level.SEVERE, null, e);
+        }
+        return idDelivery;
     }
 
+    /***
+     * Evaluate an activity in database.
+     * <p>
+     * This method is used by Professor when he/she needs evaluate an activity saved in database.
+     * </p>
+     * @param idDelivery delivery's id to evaluate
+     * @param  score the delivery score
+     * @param observation the observations
+     * @return boolean true if 1 o more than 1 rows are affected
+     */
     @Override
-    public boolean evaluateActivityDelivery(int idActivity, float score, String observation) {
-        return true;
+    public boolean evaluateDeliveryOfActivity(int idDelivery, float score, String observation) {
+        int rowsAffected = 0;
+        try(Connection conn = database.getConnection()){
+            conn.setAutoCommit(false);
+            String statement = "UPDATE Delivery SET observation = ?, score = ? WHERE id_delivery = ?";
+            PreparedStatement updateDeliveryInformation = conn.prepareStatement(statement);
+            updateDeliveryInformation.setString(1, observation);
+            updateDeliveryInformation.setFloat(2, score);
+            updateDeliveryInformation.setInt(3, idDelivery);
+            rowsAffected = updateDeliveryInformation.executeUpdate();
+            conn.commit();
+        } catch (SQLException | NullPointerException e) {
+            Logger.getLogger(DeliveryDAO.class.getName()).log(Level.SEVERE, null, e);
+        }
+        return  rowsAffected > 0;
     }
 
+    /***
+     * Get all deliveries from activities from database.
+     * <p>
+     * This method return all deliveries by activity from database.
+     * It's used by Professor.
+     * </p>
+     * @param idActivity
+     * @return List<Delivery> A list containing deliveries
+     */
     @Override
-    public List<Delivery> getAllDeliveriesByActivity() {
-        return null;
+    public List<Delivery> getAllDeliveriesByActivity(int idActivity) {
+        String statement = "SELECT DEL.id_delivery, DEL.observation, DEL.score, DEL.file, DEL.filename, ACT.id_activity, ACT.name, ACT.description, ACT.deadline, COUR.id_course, COUR.NRC, COUR.name, COUR.PERIOD, PERSPROF.name, PERSPROF.phoneNumber, PERSPROF.email, PROF.cubicle, PROF.staff_number FROM delivery AS DEL INNER JOIN Activity AS ACT ON DEL.id_activity = ? INNER JOIN Professor AS PROF ON ACT.id_professor = PROF.id_person INNER JOIN Person AS PERSPROF ON PROF.id_person = PERSPROF.id_person INNER JOIN Course AS COUR ON PERSPROF.id_course = COUR.id_course";
+        return getAllDeliveryByStatementAndID(statement, idActivity);
     }
 
     /***
@@ -49,65 +116,62 @@ public class DeliveryDAO implements IDeliveryDAO {
      */
     @Override
     public List<Delivery> getAllDeliveriesByPractitionerID(int idPractitioner) {
+        String statement = "SELECT DEL.id_delivery, DEL.observation, DEL.score, DEL.file, DEL.filename, ACT.id_activity, ACT.name, ACT.description, ACT.deadline, COUR.id_course, COUR.NRC, COUR.name, COUR.PERIOD, PERSPROF.name, PERSPROF.phoneNumber, PERSPROF.email, PROF.cubicle, PROF.staff_number FROM delivery AS DEL INNER JOIN Activity AS ACT ON DEL.id_activity = ACT.id_activity AND DEL.id_practitioner = ? INNER JOIN Professor AS PROF ON ACT.id_professor = PROF.id_person INNER JOIN Person AS PERSPROF ON PROF.id_person = PERSPROF.id_person INNER JOIN Course AS COUR ON PERSPROF.id_course = COUR.id_course";
+        return getAllDeliveryByStatementAndID(statement, idPractitioner);
+    }
+
+    private List<Delivery> getAllDeliveryByStatementAndID(String statement, int id) {
         List<Delivery> deliveries = new ArrayList<>();
         try(Connection conn = database.getConnection() ) {
-            String statement = "SELECT DEL.observation, DEL.score, DEL.file, DEL.filename, ACT.id_activity, ACT.name, ACT.description, ACT.deadline, COUR.id_course, COUR.NRC, COUR.name, COUR.PERIOD, PERSPROF.name, PERSPROF.phoneNumber, PERSPROF.email, PROF.cubicle, PROF.staff_number FROM delivery AS DEL INNER JOIN Activity AS ACT ON DEL.id_activity = ACT.id_activity AND DEL.id_practitioner = ? INNER JOIN Professor AS PROF ON ACT.id_professor = PROF.id_person INNER JOIN Person AS PERSPROF ON PROF.id_person = PERSPROF.id_person INNER JOIN Course AS COUR ON PERSPROF.id_course = COUR.id_course";
+            conn.setAutoCommit(false);
             PreparedStatement preparedStatement = conn.prepareStatement(statement);
-            preparedStatement.setInt(1, idPractitioner);
-            result = preparedStatement.executeQuery();
-            while( result.next() ) {
+            preparedStatement.setInt(1, id);
+            resultSet = preparedStatement.executeQuery();
+            while( resultSet.next() ) {
                 Course course = new Course();
-                course.setId(result.getInt("COUR.id_course"));
-                course.setName(result.getString("COUR.name"));
-                course.setNRC(result.getString("COUR.NRC"));
-                course.setPeriod(result.getString("COUR.period"));
+                course.setId(resultSet.getInt( "COUR.id_course") );
+                course.setName(resultSet.getString( "COUR.name") );
+                course.setNRC(resultSet.getString( "COUR.NRC") );
+                course.setPeriod(resultSet.getString( "COUR.period") );
                 Professor professor = new Professor();
-                professor.setName(result.getString("PERSPROF.name"));
-                professor.setPhoneNumber(result.getString("PERSPROF.phoneNumber"));
-                professor.setEmail(result.getString("PERSPROF.email"));
+                professor.setName(resultSet.getString( "PERSPROF.name") );
+                professor.setPhoneNumber(resultSet.getString( "PERSPROF.phoneNumber") );
+                professor.setEmail(resultSet.getString( "PERSPROF.email"));
                 professor.setCourse(course);
-                professor.setStaffNumber(result.getString("PROF.staff_number"));
-                professor.setCubicle(result.getInt("PROF.cubicle"));
+                professor.setStaffNumber(resultSet.getString( "PROF.staff_number") );
+                professor.setCubicle(resultSet.getInt( "PROF.cubicle") );
                 Activity activity = new Activity();
-                activity.setDeadline(result.getDate("ACT.deadline"));
-                activity.setDescription(result.getString("ACT.description"));
-                activity.setId(result.getInt("ACT.id_activity"));
-                activity.setName(result.getString("ACT.name"));
+                activity.setDeadline(resultSet.getDate( "ACT.deadline") );
+                activity.setDescription(resultSet.getString( "ACT.description") );
+                activity.setId(resultSet.getInt( "ACT.id_activity") );
+                activity.setName(resultSet.getString( "ACT.name") );
                 activity.setProfessor(professor);
                 activity.setDeliveries(null);
                 Delivery delivery = new Delivery();
                 delivery.setActivity(activity);
-                Document document = Document.valueOf(result.getString("DEL.filename").toUpperCase());
-                InputStream input = result.getBinaryStream("DEL.file");
-                File file = readBlobFromDatabase(document.toString(), input);
-                document.setFile(file);
-                delivery.setDocument(document);
-                delivery.setObservation(result.getString("DEL.observation"));
                 delivery.setPractitioner(null);
-                delivery.setScore(result.getFloat("DEL.score"));
+                delivery.setObservation( resultSet.getString("DEL.observation") );
+                delivery.setScore( resultSet.getFloat("DEl.score") );
+                delivery.setId( resultSet.getInt("DEL.id_delivery") );
+//                Document document = Document.valueOf( resultSet.getString("DEL.filename").toUpperCase() );
+//                File file = new File("hola.txt");
+//                FileOutputStream fileOutputStream = new FileOutputStream(file);
+//                InputStream input = resultSet.getBinaryStream("DEL.file");
+//                byte[] buffer = new byte[1024];
+//                while (input.read(buffer) > 0) {
+//                    fileOutputStream.write(buffer);
+//                }
+//                document.setFile(file);
+                delivery.setDocument(null);
                 deliveries.add(delivery);
+                conn.commit();
             }
-        } catch (SQLException | NullPointerException | IOException e) {
+        } catch (SQLException | NullPointerException e) {
             Logger.getLogger(DeliveryDAO.class.getName()).log(Level.WARNING, null, e);
         }
         return deliveries;
     }
 
-    @Override
-    public List<Delivery> getAllDeliveriesFromLastCourse() {
-        return null;
-    }
 
-    private File readBlobFromDatabase(String filename, InputStream input) throws IOException, SQLException {
-        File file = new File(filename.toString());
-        FileOutputStream fileOutputStream = new FileOutputStream(file);
-        if(input != null){
-            byte[] buffer = new byte[1024];
-            while (input.read(buffer) > 0) {
-                fileOutputStream.write(buffer);
-            }
-        }
-        return file;
-    }
 
 }
