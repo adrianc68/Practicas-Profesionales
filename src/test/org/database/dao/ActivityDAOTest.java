9c4129bfc24fcc4f7a41e19cd71278b4a9b31a74
Jose Luis Carvajal Pacheco <58019262+JLUISCP@users.noreply.github.com>
2020-05-08 17:31:59 -0500

    Practitioner and Professor test

--- src/test/org/database/dao/ActivityDAOTest.java
+++ src/test/org/database/dao/ActivityDAOTest.java
@@ -1,23 +1,56 @@
 package test.org.database.dao;
 
+import org.database.DatabaseProperties;
+import org.database.dao.ActivityDAO;
+import org.domain.Activity;
+import org.domain.Course;
+import org.domain.Professor;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ActivityDAOTest {
 
+    private final int idActivity = 1;
+    private final int idProfessor = 1;
+
     @Test
     void addActivity() {
+        ActivityDAO activityDAO = new ActivityDAO();
+        Course course = new Course();
+        course.setId(1);
+        Professor professor = new Professor();
+        professor.setCourse(course);
+        professor.setId(1);
+        Activity activity = new Activity();
+        activity.setName("Reporte Mensual");
+        activity.setDescription("Subir el documento correspondiente al reporte mensual");
+        activity.setDeadline(null);
+        activity.setProfessor(professor);
+        int actual = activityDAO.addActivity(activity);
+        assertNotNull(actual);
     }
 
     @Test
     void removeActivityByID() {
+        ActivityDAO activityDAO = new ActivityDAO();
+        assertTrue( activityDAO.removeActivityByID(idActivity) );
     }
 
     @Test
     void getAllActivitiesFromLastCourse() {
+        ActivityDAO activityDAO = new ActivityDAO();
+        int expected = 1;
+        int actual = activityDAO.getAllActivitiesFromLastCourse().size();
+        assertEquals(expected, actual);
     }
 
     @Test
     void getAllActivitiesByProfessor() {
+        ActivityDAO activityDAO = new ActivityDAO();
+        int expected = 1;
+        int actual = activityDAO.getAllActivitiesByProfessorID(idProfessor).size();
+        assertEquals(expected, actual);
     }
 }
--- src/test/org/database/dao/DeliveryDAOTest.java
+++ src/test/org/database/dao/DeliveryDAOTest.java
@@ -1,34 +1,61 @@
 package test.org.database.dao;
 
 import org.database.dao.DeliveryDAO;
-import org.domain.Delivery;
+import org.domain.*;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DeliveryDAOTest {
 
+    private final int idPractitioner = 1;
+    private final int idActivity = 1;
+    private final int idDelivery = 1;
+
     @Test
     void addDeliveryActivity() {
+        DeliveryDAO deliveryDAO = new DeliveryDAO();
+        Course course = new Course();
+        course.setId(1);
+        Practitioner practitioner = new Practitioner();
+        practitioner.setCourse(course);
+        practitioner.setId(1);
+        Professor professor = new Professor();
+        professor.setId(2);
+        Activity activity = new Activity();
+        activity.setId(1);
+        activity.setProfessor(professor);
+        Document document = Document.MONTHLY_REPORT;
+        Delivery delivery = new Delivery();
+        delivery.setDocument(document);
+        delivery.setActivity(activity);
+        delivery.setPractitioner(practitioner);
+        int actual = deliveryDAO.addDeliveryToActivity(delivery, idActivity);
+        assertNotNull(actual);
     }
 
     @Test
     void evaluateActivityDelivery() {
+        DeliveryDAO deliveryDAO = new DeliveryDAO();
+        String  observation = "Sin observaciones";
+        float score = 9;
+        assertTrue( deliveryDAO.evaluateDeliveryOfActivity(idDelivery, score, observation) );
     }
 
     @Test
     void getAllDeliveriesByActivity() {
+        DeliveryDAO deliveryDAO = new DeliveryDAO();
+        int expected = 1;
+        int actual = deliveryDAO.getAllDeliveriesByActivity(idActivity).size();
+        assertEquals(expected, actual);
     }
 
     @Test
     void getAllDeliveriesByPractitionerID() {
         DeliveryDAO deliveryDAO = new DeliveryDAO();
-        int expected = 3;
-        int result = deliveryDAO.getAllDeliveriesByPractitionerID(3).size();
-        assertEquals(expected, result);
+        int expected = 1;
+        int actual = deliveryDAO.getAllDeliveriesByPractitionerID(idPractitioner).size();
+        assertEquals(expected, actual);
     }
 
-    @Test
-    void getAllDeliveriesFromLastCourse() {
-    }
 }
