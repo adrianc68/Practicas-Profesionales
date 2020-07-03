d4a532a2a5ef90cd9f5eac9feee0c4b89fdfef20
Jose Luis Carvajal Pacheco <58019262+JLUISCP@users.noreply.github.com>
2020-07-03 13:00:12 -0500

    Create activity module

---
+++ src/org/gui/auth/users/professor/activity/createactivity/AddActivityController.java
@@ -0,0 +1,185 @@
+package org.gui.auth.users.professor.activity.createactivity;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import org.database.dao.ActivityDAO;
+import org.domain.Activity;
+import org.domain.Professor;
+import org.gui.Controller;
+import org.util.Auth;
+import org.util.Validator;
+import java.net.URL;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+public class AddActivityController extends Controller implements Initializable {
+    private boolean dataInputValid;
+    private boolean addOperationStatus;
+    private Activity newActivity;
+    
+    @FXML private TextField tittleTextField;
+    @FXML private TextArea descriptionTextArea;
+    @FXML private DatePicker deadlineDatePicker = new DatePicker();
+    @FXML private AnchorPane rootStage;
+    @FXML private Label systemLabel;
+    @FXML private Spinner<Integer> hourSpinner;
+    @FXML private Spinner<Integer> minuteSpinner;
+    @FXML private Spinner<Integer> secondSpinner;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //setStyleClass(rootStage, getClass().getResource("../../../resources/" + CSSProperties.readTheme().getTheme() ).toExternalForm() );
+        setListenerToTextFields();
+        setListenerToTextArea();
+        deadlineDatePicker.setEditable(false);
+        hourSpinner.setValueFactory( new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 23,12,1) );
+        minuteSpinner.setValueFactory( new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 59,0,1) );
+        secondSpinner.setValueFactory( new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 59,0,1) );
+    }
+
+    public void showStage() {
+        loadFXMLFile(getClass().getResource("/org/gui/auth/users/professor/activity/createactivity/AddActivityVista.fxml"), this);
+        stage.show();
+    }
+
+    @FXML
+    void addButtonPressed(ActionEvent event) {
+        if(deadlineDatePicker.getValue() != null){
+            if ( compareDates( deadlineDatePicker.getValue()+"-"+hourSpinner.getValue()+"-"+minuteSpinner.getValue()+"-"+secondSpinner.getValue() ) ) {
+                if (dataInputValid) {
+                    addOperationStatus = false;
+                    newActivity = new Activity();
+                    newActivity.setName(tittleTextField.getText());
+                    newActivity.setDescription(descriptionTextArea.getText());
+                    newActivity.setDeadline( deadlineDatePicker.getValue()+"-"+hourSpinner.getValue()+"-"+minuteSpinner.getValue()+"-"+secondSpinner.getValue() );
+                    newActivity.setProfessor( (Professor) Auth.getInstance().getCurrentUser() );
+                    System.out.println(newActivity);
+                    ActivityDAO activityDAO = new ActivityDAO();
+                    try {
+                        newActivity.setId(activityDAO.addActivity(newActivity));
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    closeStage(event);
+                }else{
+                    systemLabel.setText("Los campos marcados en rojo no contienen información valida");
+                }
+            }else{
+                systemLabel.setText("La fecha seleccionada no es valida");
+            }
+        }else{
+            systemLabel.setText("No se ha seleccionado una fecha");
+        }
+    }
+
+    @FXML
+    void cancelButtonPressed(ActionEvent event) {
+        addOperationStatus = false;
+        closeStage(event);
+    }
+
+    private void setListenerToTextFields() {
+        Map<Object[], TextField> textfields = new LinkedHashMap<>();
+        Object[] tittleConstraints = {Validator.NAME_PATTERN, Validator.NAME_LENGTH};
+        textfields.put(tittleConstraints, tittleTextField);
+        for (Map.Entry<Object[], TextField> entry : textfields.entrySet() ) {
+            entry.getValue().textProperty().addListener(new ChangeListener<String>() {
+                @Override
+                public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                    if( !Validator.doesStringMatchPattern( newValue, ( (String) entry.getKey()[0] ) ) || Validator.isStringLargerThanLimitOrEmpty( newValue, ( (Integer) entry.getKey()[1]) ) ){
+                        dataInputValid = false;
+                        entry.getValue().setStyle("-fx-background-color:red;");
+                    } else {
+                        dataInputValid = true;
+                        entry.getValue().setStyle("-fx-background-color:green;");
+                    }
+                }
+            });
+        }
+    }
+    private void setListenerToTextArea(){
+        Map<Object[], TextArea> textarea = new LinkedHashMap<>();
+        Object[] descriptionConstraints = {Validator.LARGE_TEXT_PATTERN, Validator.LARGE_TEXT_LENGTH};
+        textarea.put(descriptionConstraints, descriptionTextArea);
+        for (Map.Entry<Object[], TextArea> entry : textarea.entrySet() ) {
+            entry.getValue().textProperty().addListener(new ChangeListener<String>() {
+                @Override
+                public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                    if( !Validator.doesStringMatchPattern( newValue, ( (String) entry.getKey()[0] ) ) || Validator.isStringLargerThanLimitOrEmpty( newValue, ( (Integer) entry.getKey()[1]) ) ){
+                        dataInputValid = false;
+                        entry.getValue().setStyle("-fx-background-color:red;");
+                    } else {
+                        dataInputValid = true;
+                        entry.getValue().setStyle("-fx-background-color:green;");
+                    }
+                }
+            });
+        }
+    }
+
+    private void closeStage(ActionEvent event) {
+        stage.close();
+    }
+
+    private boolean compareDates(String date) {
+        String actualDate[] = String.valueOf(LocalDate.now()).split("-");
+        String actualHour[] = String.valueOf(LocalTime.now()).split(":");
+        System.out.println(LocalTime.now());
+        String splitDate[] = date.split("-");
+        System.out.println(splitDate[5]);
+        if ( Integer.parseInt(actualDate[0]) > Integer.parseInt(splitDate[0]) ) {
+            return false;
+        } else {
+            if ( Integer.parseInt(actualDate[1]) > Integer.parseInt(splitDate[1]) ) {
+                return false;
+            } else {
+                if ( Integer.parseInt(actualDate[2]) > Integer.parseInt(splitDate[2]) ) {
+                    return false;
+                } else {
+                    if ( Integer.parseInt(actualDate[2]) == Integer.parseInt(splitDate[2]) ) {
+                        if ( Integer.parseInt(actualHour[0]) > Integer.parseInt(splitDate[3]) ) {
+                            return false;
+                        } else {
+                            if ( Integer.parseInt(actualHour[0]) == Integer.parseInt(splitDate[3]) ) {
+                                if ( Integer.parseInt(actualHour[1]) > Integer.parseInt(splitDate[4]) ) {
+                                    return false;
+                                } else {
+                                    if ( Integer.parseInt(actualHour[1]) == Integer.parseInt(splitDate[4]) ) {
+                                        if ( Integer.parseInt(actualHour[2].substring(0, 2)) > Integer.parseInt(splitDate[5]) ) {
+                                            return false;
+                                        } else {
+                                            if ( Integer.parseInt(actualHour[2].substring(0, 2)) == Integer.parseInt(splitDate[5]) ) {
+                                                return false;
+                                            } else {
+                                                return true;
+                                            }
+                                        }
+                                    } else {
+                                        return true;
+                                    }
+                                }
+                            } else {
+                                return true;
+                            }
+                        }
+                    } else {
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+}
+
+
